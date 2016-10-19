0a10093af6ce94d24ba7e07f056be4d52d147029
codevv <vincentvillasis@gmail.com>
2016-10-18 21:57:50 -0500

    title now shows

--- SwiftersLocationConnect/SwiftersTableViewController.swift
+++ SwiftersLocationConnect/SwiftersTableViewController.swift
@@ -26,14 +26,14 @@ class SwiftersTableViewController: UITableViewController {
         return 0
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CellForSwifters", for: indexPath)
 
         // Configure the cell...
 
         return cell
     }
-    */
+    
 
 }
